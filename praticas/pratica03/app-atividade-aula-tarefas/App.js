4520d5c7e5155e1b2fcf1ed20aeebc64140a8a7f
carolinaarosendo <carolinaarosendo@gmail.com>
2026-08-25 21:46:40 -0300

    versao final

--- praticas/pratica03/app-atividade-aula-tarefas/App.js
+++ praticas/pratica03/app-atividade-aula-tarefas/App.js
@@ -1,7 +1,5 @@
 
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import { rotulo_btn_cadastro_meta } from './mensagem';
-import { rotulo_input_meta } from './mensagem';
+import { StyleSheet, View } from 'react-native';
 import MetaList from './components/MetaList';
 import { useState } from 'react';
 import MetaInput from './components/MetaInput';
